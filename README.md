a772a0700a00f628f9a17eaeb3760ed2b4c35413
Robert <robertnelea56@gmail.com>
2023-09-04 13:35:14 -0400

    1.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# i-plus-interactif-to-pdf
+# i-plus-interactif-to-pdf (v1.0)
 
 ## Project Overview
 
@@ -14,3 +14,20 @@ We want to make it abundantly clear that the "i-plus-interactif-to-pdf" tool is 
 Users are strongly advised to comply with all applicable laws and regulations regarding the use of copyrighted materials and to respect the terms and conditions set by the content providers. Any use of this software that violates copyright or other legal rights is entirely the responsibility of the user.
 
 We do not endorse or condone any illegal activities, and any such use of this tool is expressly discouraged.
+
+## Requirements
+Latest Python
+```
+pip install selenium
+```
+```
+pip install webdriver_manager
+```
+```
+pip install fpdf2
+```
+
+You need to add you're I+ Interactif email and password to the variables located in the begeninbg of `main()`.
+
+If you're computer is slow, you can increase the value in `time.sleep()`.
+
--- main.py
+++ main.py
@@ -5,162 +5,172 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import base64
 import os
-from datetime import datetime
+import pathlib
 from fpdf import FPDF
-from PIL import Image
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.lib.utils import ImageReader
-from reportlab.platypus import SimpleDocTemplate, PageTemplate, PageBreak, Image
-import io
 
+def main():
+    email = ""
+    password = ""
 
-email = "robertnelea56@gmail.com"
-password = "Stonksr10056!"
+    options = Options()
+    options.add_experimental_option("detach", True)
 
-options = Options()
-options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get("https://www.iplusinteractif.com/")
+    driver.maximize_window()
 
-driver.get("https://www.iplusinteractif.com/")
-driver.maximize_window()
+    emailElement = driver.find_element("xpath", "//*[@id='loginId']")
+    emailElement.click()
+    emailElement.send_keys(email)
 
-emailElement = driver.find_element("xpath", "//*[@id='loginId']")
-emailElement.click()
-emailElement.send_keys(email)
+    passwordElement = driver.find_element("xpath", "//*[@id='password']")
+    passwordElement.click()
+    passwordElement.send_keys(password)
 
-passwordElement = driver.find_element("xpath", "//*[@id='password']")
-passwordElement.click()
-passwordElement.send_keys(password)
+    sendButton = driver.find_element("xpath", "//*[contains(@class, 'blue button')]")
+    sendButton.click()
 
-sendButton = driver.find_element("xpath", "//*[contains(@class, 'blue button')]")
-sendButton.click()
+    time.sleep(5)
+    accessListElements = driver.find_elements("xpath", "//div[contains(@class, 'accessContainer')]")
+    for element in accessListElements:
+        bookName = element.find_element("xpath", ".//h2[@class='access__title']").text
+        question1 = input(f"Do you want to copy {bookName}? (yes/no):")
+        if question1 == "yes":
+            element.click()
+            time.sleep(3)
 
-time.sleep(5)
-accessListElements = driver.find_elements("xpath", "//div[contains(@class, 'accessContainer')]")
-for element in accessListElements:
-    bookName = element.find_element("xpath", ".//h2[@class='access__title']").text
-    question = input(f"Do you want to copy {bookName}? (yes/no):")
-    if question == "yes":
-        element.click()
-        time.sleep(3)
+            driver.switch_to.window(driver.window_handles[1])
 
-        driver.switch_to.window(driver.window_handles[1])
+            try:
+                closePopup = driver.find_element("xpath", "//*[@class='iplus-l-confBook__commercialPopupCloseBtnTop']")
+                closePopup.click()
+                print("popup closed")
+            except:
+                print("no popup")
+            
+            time.sleep(1)
+            openBook = driver.find_element("xpath", "//a[@class='iplus-l-confBook__itemVolumeCouv coverEffect']")
+            openBook.click()
+            time.sleep(3)
+            pageInput = driver.find_element("xpath", "//input[@class='iplus-R-ReactPreviewFrame__pagination_input']")
+            pageInput.send_keys('C1')
+            pageInput.send_keys(u'\ue007')
+            time.sleep(3)
 
-        try:
-            closePopup = driver.find_element("xpath", "//*[@class='iplus-l-confBook__commercialPopupCloseBtnTop']")
-            closePopup.click()
-            print("popup closed")
-        except:
-            print("no popup")
-         
-        time.sleep(1)
-        openBook = driver.find_element("xpath", "//a[@class='iplus-l-confBook__itemVolumeCouv coverEffect']")
-        openBook.click()
-        time.sleep(3)
-        pageInput = driver.find_element("xpath", "//input[@class='iplus-R-ReactPreviewFrame__pagination_input']")
-        pageInput.send_keys('C1')
-        pageInput.send_keys(u'\ue007')
-        time.sleep(3)
+            # Create the directory if it doesn't exist
+            if not os.path.exists("imgs"):
+                os.makedirs("imgs")
 
-        image = driver.find_element("xpath", "//img[@class='iplus-R-ReactPreviewFrame__page ']")
-        img_src = image.get_attribute("src")
+            img_count = 1
+            while True:
 
-        driver.execute_script(f'''window.open("{img_src}","_blank");''')
-        driver.switch_to.window(driver.window_handles[2])
-        time.sleep(1)
-        page = driver.find_element("xpath", "//img")
-        page.click()
-        time.sleep(1)
-        js = '''// Select the image element on the page
-                var imgElement = document.querySelector('img');
+                image = driver.find_element("xpath", "//img[@class='iplus-R-ReactPreviewFrame__page ']")
+                img_src = image.get_attribute("src")
 
-                // Create a canvas element to draw the image
-                var canvas = document.createElement('canvas');
-                canvas.width = imgElement.width;
-                canvas.height = imgElement.height;
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(imgElement, 0, 0, imgElement.width, imgElement.height);
+                driver.execute_script(f'''window.open("{img_src}","_blank");''')
+                driver.switch_to.window(driver.window_handles[2])
+                time.sleep(1)
+                page = driver.find_element("xpath", "//img")
+                page.click()
+                time.sleep(0.3)
+                js = '''// Select the image element on the page
+                        var imgElement = document.querySelector('img');
 
-                // Get the base64 data URL
-                var base64Data = canvas.toDataURL('image/png'); // You can change the format as needed
+                        // Create a canvas element to draw the image
+                        var canvas = document.createElement('canvas');
+                        canvas.width = imgElement.width;
+                        canvas.height = imgElement.height;
+                        var ctx = canvas.getContext('2d');
+                        ctx.drawImage(imgElement, 0, 0, imgElement.width, imgElement.height);
 
-                return base64Data
-            '''
-        base64_img = driver.execute_script(js)
+                        // Get the base64 data URL
+                        var base64Data = canvas.toDataURL('image/png'); // You can change the format as needed
 
-        # Extract the base64 data part (after the comma)
-        base64_data = base64_img.split(',')[1]
+                        return base64Data
+                    '''
+                base64_img = driver.execute_script(js)
 
-        # Decode the base64 data
-        image_data = base64.b64decode(base64_data)
+                # Extract the base64 data part (after the comma)
+                base64_data = base64_img.split(',')[1]
 
-        
-        # Create the directory if it doesn't exist
-        if not os.path.exists("output"):
-            os.makedirs("output")     
+                # Decode the base64 data
+                image_data = base64.b64decode(base64_data)
 
-        # Specify the file name and format (e.g., 'output.png' for PNG)
-        file_path = os.path.join("output",f"{bookName}.png" )
-    
-        # Save the image to a file
-        with open(file_path, 'wb') as f:
-            f.write(image_data)
+                
+                # Specify the file name and format (e.g., 'output.png' for PNG)
+                file_path = os.path.join("imgs",f"{img_count}.png" )
+            
+                # Save the image to a file
+                with open(file_path, 'wb') as f:
+                    f.write(image_data)
 
-        print(f'Image saved as {bookName} in {file_path}')
+                print(f'Page #{img_count} saved in {file_path}')
 
+                driver.close()
+                driver.switch_to.window(driver.window_handles[1])
 
-        #create pdf
-        # Create a PDF document
-        c = canvas.Canvas(f"{bookName}.pdf", pagesize=letter)
+                try:
+                    next_element = driver.find_element("xpath", "//div[@class='sc-hKMtZM ehqEaE iplus-l-ReactPreviewFrame__paginationArrow__arrowRight']")
+                except:
+                    print(f"{img_count} pages from {bookName} was copied successfully!")
+                    break
+                driver.execute_script("arguments[0].click();", next_element)
 
-        # Create an image reader from the decoded image data
-        img_reader = ImageReader(io.BytesIO(image_data))
+                img_count += 1
 
-        # Get image dimensions using getSize() method
-        img_width, img_height = img_reader.getSize()
+                time.sleep(3)
 
-        # Calculate the aspect ratio
-        aspect_ratio = float(img_width) / float(img_height)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            driver.quit()
+            png_to_pdf(bookName, img_count)
 
-        # Set the page size while maintaining the aspect ratio
-        if aspect_ratio >= 1.0:
-            page_width = letter[0]
-            page_height = letter[0] / aspect_ratio
         else:
-            page_height = letter[1]
-            page_width = letter[1] * aspect_ratio
+            print(f"{bookName} was successfully skiped.")
+        time.sleep(1)
 
-        c.setPageSize((page_width, page_height))
+def png_to_pdf(bookName, img_count):
+    question = input("What would you prefer?\nMake a pdf with the copied pages (1)\nKeep the copied pages as a directory of images (2)\n\nAnswer (1 or 2):")
 
-        # Calculate the scaling factor to fit the entire image on the page
-        scaling_factor = min(page_width / img_width, page_height / img_height)
+    if question == "1":
 
-        # Calculate the image dimensions with scaling
-        img_width_scaled = img_width * scaling_factor
-        img_height_scaled = img_height * scaling_factor
+        print("loading ...")
 
-        # Center the image on the page
-        x_offset = (page_width - img_width_scaled) / 2
-        y_offset = (page_height - img_height_scaled) / 2
+        # Initialize a list to store image file paths
+        image_files = []
 
-        # Draw the image on the page with scaling
-        c.drawImage(img_reader, x_offset, y_offset, img_width_scaled, img_height_scaled)
+        # Iterate through the files in the directory and add numeric names to the list
+        for filename in os.listdir("imgs"):
+            if filename.endswith(".png") and filename[:-4].isnumeric():
+                image_files.append(os.path.join("imgs", filename))
 
-        # # Add a new page for the next image (if not the last one)
-        # if base64_image_data != base64_images[-1]:
-        c.showPage()
-
-        c.save()
-
-        driver.close()
-        driver.switch_to.window(driver.window_handles[1])
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        
+        # Sort the image file paths based on their names (which are assumed to be integers)
+        image_files.sort(key=lambda x: int(os.path.basename(x)[:-4]))
 
 
+        # test_images = [ os.path.join("imgs", "1.png"), os.path.join("imgs", "2.png"), os.path.join("imgs", "3.png")]
+        # # imagelist is the list with all image filenames
+        # print(test_images)
+
+        pdf = FPDF()
+
+        for image in image_files:
+            pdf.add_page("P", (2640, 3263))
+            pdf.image(image, 0, 0, 2640, 3263)
+        pdf.output(f"{bookName}.pdf", "F")
+
+        print(f"PDF file '{bookName}' created successfully.")
+        pathlib.Path.rmdir("imgs") 
+    elif question == "2":
+        try:
+            os.rename("imgs", bookName)
+            print(f"You can find {img_count} pages in /{bookName}")
+        except:
+            os.rename("imgs", "book")
+            print(f"Your book has a weird name.\nYou can find {img_count} pages in /book")
     else:
-        print(f"{bookName} was successfully skiped.")
-    time.sleep(1)
+        print("Chose an option between 1 and 2")
+        png_to_pdf()
+
+main()
